882653c56993f38a2477327e9dcbf5dfdab4f51f
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-09-05 21:58:36 -0400

    Base de Datos.

---
+++ app/src/main/java/edu/ucne/registrojugador/data/JugadorDatabase.kt
@@ -0,0 +1,19 @@
+package edu.ucne.registrojugador.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.registrojugador.data.local.entities.JugadorEntity
+import edu.ucne.registrojugador.data.local.entities.dao.JugadorDao
+
+
+@Database(
+    entities = [
+        JugadorEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+abstract  class JugadorDatabase : RoomDatabase(){
+    abstract fun JugadorDao(): JugadorDao
+}
