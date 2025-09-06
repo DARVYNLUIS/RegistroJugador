956a4540d543bfa3b8d42d2f9877ad8877a29a39
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-09-05 22:01:27 -0400

    Ultimos cambios.

---
+++ app/src/main/java/edu/ucne/registrojugador/Presentation/EditJugadorUiEvent.kt
@@ -0,0 +1,9 @@
+package edu.ucne.registrojugador.presentation.jugador.edit
+
+sealed interface EditJugadorUiEvent {
+    data class Load(val id: Int?) : EditJugadorUiEvent
+    data class NombresChanged(val value: String) : EditJugadorUiEvent
+    data class PartidasChanged(val value: String) : EditJugadorUiEvent
+    data object Save : EditJugadorUiEvent
+    data object Delete : EditJugadorUiEvent
+}
