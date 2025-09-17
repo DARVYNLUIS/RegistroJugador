897c40b2b0318cfb541676d5e9f19382cd491b20
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-09-17 13:18:38 -0400

    Creando entidad partida

---
+++ app/src/main/java/edu/ucne/registrojugador/data/local/entities/Partida.kt
@@ -0,0 +1,16 @@
+package edu.ucne.registrojugador.data.local.entities
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Partidas")
+data class PartidaEntity(
+    @PrimaryKey(autoGenerate = true)
+    val partidaId: Int = 0,
+    val fecha: String,
+    val jugador1Id: Int,
+    val jugador2Id: Int,
+    val ganadorId: Int? = null,
+    val esFinalizada: Boolean = false
+)
