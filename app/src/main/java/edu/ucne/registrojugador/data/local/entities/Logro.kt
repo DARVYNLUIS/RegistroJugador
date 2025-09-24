8a8255e12fd9cb1de8025e36eec531a09316383e
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-09-24 11:41:51 -0400

    Creando entidad logro.

---
+++ app/src/main/java/edu/ucne/registrojugador/data/local/entities/Logro.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registrojugador.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Logros")
+data class LogroEntity(
+    @PrimaryKey(autoGenerate = true)
+    val logroId: Int = 0,
+    val nombre: String,
+    val descripcion: String,
+)
