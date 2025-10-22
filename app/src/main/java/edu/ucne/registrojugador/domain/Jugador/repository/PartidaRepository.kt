3ad9b842f83f41c44f1ef823768b6f7a710bc5e7
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-10-22 15:17:30 -0400

    Cmbios en partida Repository

--- app/src/main/java/edu/ucne/registrojugador/domain/Jugador/repository/PartidaRepository.kt
+++ app/src/main/java/edu/ucne/registrojugador/domain/Jugador/repository/PartidaRepository.kt
@@ -1,14 +1,18 @@
-// file: edu/ucne/registrojugador/domain/jugador/repository/PartidaRepository.kt
-
 package edu.ucne.registrojugador.domain.jugador.repository
 
 import edu.ucne.registrojugador.domain.jugador.model.Partida
 import kotlinx.coroutines.flow.Flow
 
 interface PartidaRepository {
+    // --- Room ---
     fun getPartidas(): Flow<List<Partida>>
     suspend fun getPartidaById(id: Int): Partida?
     suspend fun insertar(partida: Partida)
     suspend fun actualizar(partida: Partida)
-    suspend fun eliminar(partida: Partida) // Corrected: This should be a domain model
+    suspend fun eliminar(partida: Partida)
+    suspend fun crearPartida(jugador1Id: Int, jugador2Id: Int): Partida
+
+    // --- API ---
+    suspend fun getPartidasFromApi(): List<Partida>
+    suspend fun crearPartidaApi(jugador1Id: Int, jugador2Id: Int): Partida
 }
