cdabc4b46df023de9074a22079f4ecd856b3320b
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-10-29 15:26:42 -0400

    Dto Creado.

--- app/src/main/java/edu/ucne/registrojugador/data/local/dto/MovimientoDTO.kt
+++ app/src/main/java/edu/ucne/registrojugador/data/local/dto/MovimientoDTO.kt
@@ -1,18 +1,11 @@
 package edu.ucne.registrojugador.data.local.dto
 
 
-
-data class PartidaDto(
-    val partidaId: Int = 0,
-    val jugador1Id: Int = 0,
-    val jugador2Id: Int = 0
-)
-
-
-data class Movimiento(
+data class MovimientoDto(
     val partidaId: Int,
     val jugador: String,
     val posicionFila: Int,
     val posicionColumna: Int
 )
+
 
